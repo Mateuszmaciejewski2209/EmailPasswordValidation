847ecd5e7b44f28c4815b106c4d2b1b98f9079be
Mateusz Maciejewski <101874030+Mateuszmaciejewski2209@users.noreply.github.com>
2024-09-05 09:08:54 +0200

    comments
    added comments explaining individual elements of the class and methods

--- UserValidator.php
+++ UserValidator.php
@@ -1,6 +1,19 @@
 <?php
 
+/**
+ * Class UserValidator
+ * is used to validate users email and password using regular expressions
+ */
 class UserValidator {
+    /**
+     * Validate email
+     * /^[a-zA-Z0-9._%+-] - checks for a string that stands before the @ symbol
+     * +@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ - checks for a string and domain that stands after the @ symbol
+     * 
+     * preg_match() - performs a regular expression match
+     * @param string $email
+     * @return bool
+     */
     public function validateEmail(string $email): bool {
         $pattern = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
 
@@ -11,6 +24,18 @@ class UserValidator {
         }
     }
 
+    /**
+     * Validate password
+     * ?=.*[a-z] - checks for at least one lowercase letter
+     * ?=.*[A-Z] - checks for at least one uppercase letter
+     * ?=.*\d - checks for at least one digit
+     * ?=.*[@$!%*?&] - checks for at least one special character
+     * [A-Za-z\d@$!%*?&]{8,} - checks for at least 8 characters from the mentioned character set
+     * 
+     * preg_match() - performs a regular expression match
+     * @param string $password
+     * @return bool
+     */
     public function validatePassword(string $password): bool {
         $pattern = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/';
 
