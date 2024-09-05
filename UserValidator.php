ee493892f52ed4db4a325586f7ba703392bcdc36
Mateusz Maciejewski <101874030+Mateuszmaciejewski2209@users.noreply.github.com>
2024-09-05 08:56:05 +0200

    created class UserValidator and its methods
    class UserValidator with two public methods - validateEmail and validatePassword

---
+++ UserValidator.php
@@ -0,0 +1,12 @@
+<?php
+
+class UserValidator {
+    public function validateEmail(string $email): bool {
+        return true;
+    }
+
+    
+    public function validatePassword(string $password): bool {
+        return true;
+    }
+}
